5cce9dea4fa2177eee4dfe9ca904d5802f31ac80
Gustavo Souza <gustavoelia7@gmail.com>
2024-07-17 11:58:58 -0300

    connect to mysql using environment variables

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import routes from "./src/routes/notes.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
